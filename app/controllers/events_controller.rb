31baf32de95e79663a80e77616cd1e4e0058cf57
AyakaOoshima <och1215bi3@gmail.com>
2022-11-08 07:52:25 +0900

    グローバルナビリンク変更

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,26 @@ class EventsController < ApplicationController
     end
 
     def new
-        @event = Event.new
+        @event = Event.new(flash[:event])
+    end
+
+    def create
+        event = Event.new(event_params)
+        if event.save
+            flash[:notice] = "「#{event.title}」の掲示板を作成しました。"
+            redirect_to events_path
+        else
+            redirect_to :back, flash: {
+                event: event,
+                erro_messages: event.errors.full_messages
+            }
+        end
+    end
+
+    private
+
+    def event_params
+        # TODO：FORM整えたらカラム増やす
+        params.require(:event).permit(:title, :content)
     end
 end
