f8eac683c06eb6b4b757b5672b8f4b0bc700ba33
AyakaOoshima <och1215bi3@gmail.com>
2022-11-10 20:29:55 +0900

    ログイン画面をrootに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  # rootをログイン画面に設定する
+  devise_scope :user do
+    root "users/sessions#new"
+  end
+
+  devise_for :users, :controllers => {
+    sessions: 'users/sessions'
+  }
+
   resources :events
 end
