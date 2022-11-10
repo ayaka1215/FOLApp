e2870a8e7f692d28e800ab327c1acd7fe972552c
AyakaOoshima <och1215bi3@gmail.com>
2022-11-11 07:38:34 +0900

    editアクション、destroyアクション定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,12 @@ class UsersController < ApplicationController
     def me
         @user = User.find(current_user.id)
     end
+
+    def edit
+        
+    end
+
+    def destroy
+        
+    end
 end
