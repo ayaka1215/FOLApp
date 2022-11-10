cba775c616f640d1085e5967d4fa5495c0f505e6
AyakaOoshima <och1215bi3@gmail.com>
2022-11-10 09:45:14 +0900

    ログイン画面をルートパスへ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    root "users/sessions#new"
+  end
+
+  devise_for :users, :controllers => {
+    sessions: 'users/sessions'
+  }
   resources :events
 end
