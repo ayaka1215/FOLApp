72412016419649c56256b1229e67777afcf946c8
AyakaOoshima <och1215bi3@gmail.com>
2022-11-10 16:57:42 +0900

    ログイン先切り分け

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
     def after_sign_in_path_for(resource)
-        events_path 
+        if current_user.admin
+            admin_events_path
+        else
+            events_path
+        end
+         
     end
 
     def after_sign_out_path_for(resource)
