62b179617c18ac0345110031d2a6209baec88ef1
AyakaOoshima <och1215bi3@gmail.com>
2022-11-10 19:58:04 +0900

    admin関連削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     def after_sign_in_path_for(resource)
-        if current_user.admin
-            admin_events_path
-        else
             events_path
-        end
-         
     end
 
     def after_sign_out_path_for(resource)
