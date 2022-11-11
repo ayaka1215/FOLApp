14d93d60f20f3b1b50bc02cf178b8c24e87065c1
AyakaOoshima <och1215bi3@gmail.com>
2022-11-12 08:36:58 +0900

    不具合修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reservations, dependent: :destroy
+
+  # partsテーブル関連
+  has_many :user_part_relations, dependent: :delete_all
+  has_many :parts, through: :user_part_relations
 end
