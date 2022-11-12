3f485055f01e1d156ec3158b34279414369d7332
AyakaOoshima <och1215bi3@gmail.com>
2022-11-12 09:33:06 +0900

    バリデーション

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ApplicationRecord
     has_many :reservations
+
+    validates :title, presence: true, length: { maximum: 30 }
+    validates :event_date, presence: true
+    validates :event_start_time, presence: true
+    validates :event_end_time, presence: true
+    validates :content, presence: true, length: { maximum: 1000 }
+    validates :place, presence: true, length: { maximum: 50 }
 end
