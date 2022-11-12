a1bd05965f69c16ec069beb320c6dc10ae4ef8c5
AyakaOoshima <och1215bi3@gmail.com>
2022-11-12 09:51:51 +0900

    end_check、start_check追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,14 @@ class Event < ApplicationRecord
     validates :event_end_time, presence: true
     validates :content, presence: true, length: { maximum: 1000 }
     validates :place, presence: true, length: { maximum: 50 }
+    validate :end_check
+    validate :start_check
+
+    def end_check
+      errors.add(:event_end_time, "は開始時刻より遅い時間を選択してください") if self.event_start_time > self.event_end_time
+    end
+  
+    def start_check
+      errors.add(:event_start_time, "は現在の日時より遅い時間を選択してください") if self.event_start_time < Time.now
+    end
 end
