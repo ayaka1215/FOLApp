6e639abe8b2a3b956e25e33bc78635cc0fe27dad
AyakaOoshima <och1215bi3@gmail.com>
2022-11-13 07:59:21 +0900

    タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module FolApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # タイムゾーン設定
+    config.time_zone = 'Tokyo'
   end
 end
