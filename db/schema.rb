5b765e7b393e3673881dc151923aade54dce568e
AyakaOoshima <och1215bi3@gmail.com>
2022-11-13 19:02:35 +0900

    imageカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_234249) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_100205) do
   create_table "events", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.date "event_date"
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_11_234249) do
     t.string "place"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "parts", charset: "utf8mb4", force: :cascade do |t|
