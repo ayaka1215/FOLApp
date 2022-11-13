76daa6ead47aeea6f3e9adee30eacc96b1b73890
AyakaOoshima <och1215bi3@gmail.com>
2022-11-13 17:33:18 +0900

    不具合修正

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module FolApp
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.time_zone = 'Tokyo'
-    config.i18n.default.locale = :ja
+    config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
   end
 end
