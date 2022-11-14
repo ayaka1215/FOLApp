8c025ee2fb61ce69e0af2f33127d090b5df750ee
AyakaOoshima <och1215bi3@gmail.com>
2022-11-14 11:52:35 +0900

    バリデーション削除（書き直し）

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,15 +7,15 @@ class Event < ApplicationRecord
     validates :event_end_time, presence: true
     validates :content, presence: true, length: { maximum: 1000 }
     validates :place, presence: true, length: { maximum: 50 }
-    validate :end_check
-    validate :start_check
+    # validate :end_check
+    # validate :start_check
     mount_uploader :image, ImageUploader
 
-    def end_check
-      errors.add(:event_end_time, "は開始時刻より遅い時間を選択してください") if self.event_start_time > self.event_end_time
-    end
+    # def end_check
+    #   errors.add(:event_end_time, "は開始時刻より遅い時間を選択してください") if self.event_start_time > self.event_end_time
+    # end
   
-    def start_check
-      errors.add(:event_start_time, "は現在の日時より遅い時間を選択してください") if self.event_start_time < Time.now
-    end
+    # def start_check
+    #   errors.add(:event_date, "は現在の日時より遅い日時を選択してください") if self.event_date < Time.now
+    # end
 end
