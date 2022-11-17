ccf77efe404ba4b18856b77b6060a7c57b0aea52
AyakaOoshima <och1215bi3@gmail.com>
2022-11-17 16:44:34 +0900

    usersカラム変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :authenticate_user!
     def after_sign_in_path_for(resource)
             events_path
@@ -7,4 +8,8 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resource)
         root_path
     end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+      end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_104006) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_071455) do
   create_table "events", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.date "event_date"
@@ -57,6 +57,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_104006) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.string "image"
+    t.string "name"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
