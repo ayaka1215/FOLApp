ffa78cbf99e96df528cee75ad852841c786bf6d1
AyakaOoshima <och1215bi3@gmail.com>
2022-11-19 17:08:36 +0900

    予約済みバリデーション

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,10 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates :user_id, uniqueness: { scope: :event_id,
+    message: "予約済みです"
+  }
+
+  # TOBE：予約期限バリデーション（イベント開始1週間前）
 end
