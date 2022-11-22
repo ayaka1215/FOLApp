597da3f22c10bd7929646e3664ac1f1b8055358d
AyakaOoshima <och1215bi3@gmail.com>
2022-11-22 19:55:23 +0900

    timeformat変更

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,3 @@
-Time::DATE_FORMATS[:datetime_jp] = '%Y年%m月%d日'
+Time::DATE_FORMATS[:datetime_stamp] = '%Y年%m月%d日'
+Time::DATE_FORMATS[:time_stamp] = '%H時%M分'
+Date::DATE_FORMATS[:date_stamp] = '%Y年%m月%d日'
