387b22d078b250066ac4b3adc68a7dbaa5b62faf
AyakaOoshima <och1215bi3@gmail.com>
2022-11-28 23:18:36 +0900

    バリデーション（エラー）

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,15 +7,20 @@ class Event < ApplicationRecord
     validates :end_time, presence: true
     validates :content, presence: true, length: { maximum: 1000 }
     validates :place, presence: true, length: { maximum: 50 }
-    # validate :end_check
-    # validate :start_check
+    validate :time_check
+    # validate :date_check
     mount_uploader :image, ImageUploader
 
-    # def end_check
-    #   errors.add(:end_time, "は開始時刻より遅い時間を選択してください") if self.start_time > self.end_time
-    # end
+    # end_timeはstart_timeより遅い時間
+    def time_check
+        if self.start_time > self.end_time
+            errors.add("終了時刻は開始時刻より遅い時間を選択してください")
+        end
+    end
   
-    # def start_check
-    #   errors.add(:date, "は現在の日時より遅い日時を選択してください") if self.date < Time.now
+    # def date_check
+    #     if self.date < Date.today + 3
+    #         errors.add("イベントを登録・変更できるのは開始日の3日前までです。")
+    #     end
     # end
 end
