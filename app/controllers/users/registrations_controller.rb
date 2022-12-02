cead8ed14a44cbee7b51cc1734a168bf427ef414
AyakaOoshima <och1215bi3@gmail.com>
2022-12-02 11:40:09 +0900

    create リダイレクト先変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -23,10 +23,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if resource.persisted?
       if resource.active_for_authentication?
         set_flash_message! :notice, :signed_up
-        # sign_up(resource_name, resource)
-        # current_user = 新規作成を行っているユーザー
         sign_up(resource_name, current_user)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        respond_with resource, location: users_path
       else
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
@@ -51,7 +49,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # DELETE /resource
   def destroy
-    super
+    binding.pry
+    resource.destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message! :notice, :destroyed
+    binding.pry
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
   end
 
   # GET /resource/cancel
