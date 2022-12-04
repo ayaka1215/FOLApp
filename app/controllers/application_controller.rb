e05221dc2be1073f2d4038cabce0ea6ced5c6a67
AyakaOoshima <och1215bi3@gmail.com>
2022-12-04 19:11:01 +0900

    不具合修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_account_update_parameters, if: :devise_controller?
     before_action :authenticate_user!
     def after_sign_in_path_for(resource)
         events_path
@@ -9,9 +10,16 @@ class ApplicationController < ActionController::Base
         root_path
     end
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :admin])
+    def after_sign_up_path_for(resource)
+        users_path
     end
 
+    private
+    def configure_account_update_parameters
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :admin])
+    end
 end
