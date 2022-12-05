df61701c3e9758ac9c5bce42fa9aed8e501811e0
AyakaOoshima <och1215bi3@gmail.com>
2022-12-05 15:38:39 +0900

    余計なコメント削除

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,23 +4,9 @@ class Event < ApplicationRecord
     validates :title, presence: true, length: { maximum: 40 }
     validates :date, presence: true
     validates :start_time, presence: true
-    validates :end_time, presence: true
+    validates :end_time, presence: true, comparison: { greater_than: :start_time }
     validates :content, presence: true, length: { maximum: 1000 }
     validates :place, presence: true, length: { maximum: 50 }
-    validate :time_check
-    # validate :date_check
     mount_uploader :image, ImageUploader
-
-    # end_timeはstart_timeより遅い時間
-    def time_check
-        if self.start_time > self.end_time
-            errors.add("終了時刻は開始時刻より遅い時間を選択してください")
-        end
-    end
-  
-    # def date_check
-    #     if self.date < Date.today + 3
-    #         errors.add("イベントを登録・変更できるのは開始日の3日前までです。")
-    #     end
-    # end
+   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # ユーザー自身でアカウント作成不可
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reservations, dependent: :destroy
 
-  # partsテーブル関連
   has_many :user_part_relations, dependent: :delete_all
   has_many :parts, through: :user_part_relations
 
