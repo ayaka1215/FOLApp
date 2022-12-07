001d1ff562eeba727f8e3d33680a6b5ff5653af5
AyakaOoshima <och1215bi3@gmail.com>
2022-12-07 15:09:46 +0900

    destroyアクション削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,12 @@ class UsersController < ApplicationController
     before_action :set_me, only: %i[edit update]
     before_action :set_target_user, only: %i[show destroy]
     before_action :is_admin, only: %i[index destroy]
+
     def index
-        # ユーザー一覧
         @users = User.all
     end
 
     def me
-        # マイページ
         @user = User.find(current_user.id)
     end
 
@@ -19,24 +18,13 @@ class UsersController < ApplicationController
     def edit
     end
 
+    # TODO：registrationコントローラに移す
     def update
         if @user.update(user_params)
             flash[:notice] = "プロフィールを修正しました。"
             redirect_to mypage_path
         else
             redirect_to mypage_path, flash: {
-                user: @user,
-                alert: @user.errors.full_messages
-            }
-        end
-    end
-
-    def destroy
-        if @user.destroy
-            flash[:notice] = "ユーザー#{@user.name}を削除しました"
-            redirect_to users_path
-        else
-            redirect_to users_path, flash: {
                 user: @user,
                 alert: @user.errors.full_messages
             }
