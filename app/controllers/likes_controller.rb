ea32776e8909b4efa2fc1415e53b0ebd8be7c4fd
AyakaOoshima <och1215bi3@gmail.com>
2022-12-28 23:35:50 +0900

    レイアウト

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,16 @@ class LikesController < ApplicationController
     before_action :authenticate_user!, only: [:create, :destroy]
 
     def create
-        like = Like.new(user_id: current_user.id, event_id: params[:format])
-        like.save
+        @like = Like.new(user_id: current_user.id, event_id: params[:format])
+        @like.save
         @event = Event.find(params[:format])
         render formats: :js, layout: false
     end
     
     def destroy
-        like = Like.find_by(user_id: current_user.id, event_id: params[:id])
-        like.destroy
+        @like = Like.find_by(user_id: current_user.id, event_id: params[:id])
+        @like.destroy
+        @event = Event.find(params[:id])
         render formats: :js, layout: false
     end
 end
