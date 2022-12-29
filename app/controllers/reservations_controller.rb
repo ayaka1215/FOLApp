bd7d3ee87060ccd1d59ed9b9b1116918af6f1d4b
AyakaOoshima <och1215bi3@gmail.com>
2022-12-29 18:40:29 +0900

    画面遷移先変更

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,7 +9,7 @@ class ReservationsController < ApplicationController
         reservation = Reservation.new(reservation_params)
         if reservation.save
             flash[:notice] = "イベント#{reservation.event.title}を予約しました。"
-            redirect_to events_path
+            redirect_to reservations_path
         else
             redirect_to events_path, flash: {
                 reservation: reservation,
@@ -22,7 +22,7 @@ class ReservationsController < ApplicationController
         reservation = Reservation.find(params[:id])
        if reservation.destroy
         flash[:notice] = "イベント#{reservation.event.title}の予約を取り消しました。"
-        redirect_to event_path        
+        redirect_to reservations_path
        else
         redirect_to events_path, flash: {
             reservation: reservation,
