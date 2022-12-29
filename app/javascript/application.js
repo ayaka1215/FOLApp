73993183f6303ee84dc978c10d889aba8fd59428
AyakaOoshima <och1215bi3@gmail.com>
2022-12-30 01:22:28 +0900

    turbo有効化

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
 import "controllers"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = true;
 
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
